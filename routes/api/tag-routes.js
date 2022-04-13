ff016ad4a82a3eb4080bc81cf4927c1a5015d023
James Gorodesky <jmg5369@gmail.com>
2022-04-12 19:15:57 -0600

    Finished routes and seeded Database

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -4,24 +4,90 @@ const { Tag, Product, ProductTag } = require('../../models');
 // The `/api/tags` endpoint
 
 router.get('/', (req, res) => {
+  try {
+    const data = await Tag.findAll({
+      include: [{ model: Product}],
+    });
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // find all tags
   // be sure to include its associated Product data
 });
 
 router.get('/:id', (req, res) => {
+  try {
+    const data = await Tag.findByPk(req.params.id, {
+      include: [{ model: Product}],
+    });
+
+    if (!data) {
+      res.status(404).json({ message: 'No tag found with that id!' });
+      return;
+    }
+
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+
   // find a single tag by its `id`
   // be sure to include its associated Product data
 });
 
 router.post('/', (req, res) => {
+  try {
+    const data = await Tag.create(
+      req.body
+    );
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(400).json(err);
+  }
   // create a new tag
 });
 
 router.put('/:id', (req, res) => {
+  try {
+    const data = await Tag.update(
+      {
+        tag_name: req.body.tag_name
+      },
+       {
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!data[0]) {
+      res.status(404).json({ message: 'No tag with this id!' });
+      return;
+    }
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // update a tag's name by its `id` value
 });
 
 router.delete('/:id', (req, res) => {
+  try {
+    const data = await Tag.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!data) {
+      res.status(404).json({ message: 'No Tag found with that id!' });
+      return;
+    }
+
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+
   // delete on tag by its `id` value
 });
 
